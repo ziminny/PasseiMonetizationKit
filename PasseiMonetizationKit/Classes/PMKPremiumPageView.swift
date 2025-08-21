d39621af5bc42210bbce837224bbd6a0a473f413
Vagner Oliveira <ziminny@gmail.com>
2025-08-21 20:55:33 -0300

    update layout ui payment

--- PasseiMonetizationKit/Classes/PMKPremiumPageView.swift
+++ PasseiMonetizationKit/Classes/PMKPremiumPageView.swift
@@ -9,304 +9,6 @@
 import SwiftUI
 import StoreKit
 
-public struct PMKPremiumPageView: View {
-    
-    @StateObject private var store = PMKStoreFacade.shared.subscriptionService
-    @Environment(\.dismiss) private var dismiss
-    
-    private let imageName: String
-    private var onTap: (Result<StoreKit.Transaction, Error>) -> Void
-    
-#if DEBUG
-    public init(imageName: String, previewType: PreviewsMoetizationType, onTap: @escaping (Result<StoreKit.Transaction, Error>) -> Void) {
-        self.imageName = imageName
-        self.onTap = onTap
-        store.previewProducts = previewType.makeProducts()
-    }
-#else
-    public init(imageName: String, onTap: @escaping (Result<StoreKit.Transaction, Error>) -> Void) {
-        self.imageName = imageName
-        self.onTap = onTap
-    }
-#endif
-    
-    public var body: some View {
-        GeometryReader { geometry in
-            
-            ZStack {
-                Image(imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: geometry.size.width)
-                    .clipped()
-                    .ignoresSafeArea()
-                
-                Rectangle()
-                    .ignoresSafeArea()
-                    .foregroundStyle(.black)
-                    .opacity(0.5)
-                
-                // Gradiente para dar cor interna à letra
-                LinearGradient(
-                    colors: [
-                        .white.opacity(0.1),
-                        .black.opacity(0.1)
-                    ],
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
-                )
-                .background(.ultraThinMaterial) // fundo translúcido
-                .blur(radius: 0.2) // vidro levemente fosco
-                .ignoresSafeArea()
-                
-                VStack {
-                    HStack(alignment: .center) {
-                        Image(systemName: "xmark.square.fill")
-                            .font(.title)
-                            .foregroundStyle(.white.opacity(0.5))
-                            .onTapGesture {
-                                dismiss()
-                            }
-                        
-                        Spacer()
-                        
-                        Text("Restaurar")
-                            .font(.headline)
-                            .fontWeight(.light)
-                            .foregroundStyle(.white)
-                            .underline()
-                    }
-                    .padding(.horizontal)
-                    
-                    Spacer(minLength: 16)
-                    advantages()
-                        .padding(.horizontal)
-                    Spacer(minLength: 48)
-                    
-                    ScrollViewReader { proxy in
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            HStack(spacing: 8) {
-                                
-                                if PREVIEWS {
-                                    #if DEBUG
-                                    ForEach(Array(store.previewProducts.enumerated()), id: \.offset) { index, plan in
-                                        VStack(spacing: 16) {
-                                            
-                                            // Precisa pensar em como fazer isso em producao
-                                            if let hot = plan.hot {
-                                                RoundedRectangle(cornerRadius: 12)
-                                                    .fill(Color.green)
-                                                    .frame(width: 90, height: 32)
-                                                    .overlay(
-                                                        Text(hot.desc)
-                                                            .font(.headline)
-                                                            .foregroundStyle(.white)
-                                                        ,alignment: .center
-                                                    )
-                                            }
-                                            
-                                            Text(plan.displayName)
-                                                .font(.title2)
-                                                .fontWeight(.semibold)
-                                                .foregroundColor(.black)
-                                            
-                                            
-                                            Rectangle()
-                                                .fill(Color.yellow)
-                                                .frame(width: 60, height: 2)
-                                            
-                                            Text(plan.displayPrice)
-                                                .font(.system(size: 40, weight: .bold))
-                                                .foregroundColor(.black)
-                                            
-                                            Text(plan.description)
-                                                .foregroundColor(.gray)
-                                                .multilineTextAlignment(.center)
-                                                .lineLimit(nil)
-                                            
-                                            Button(action: {
-                                                // no action preview
-                                            }) {
-                                                Text("Assinar")
-                                                    .fontWeight(.semibold)
-                                                    .padding()
-                                                    .frame(maxWidth: .infinity)
-                                                    .background(Color.purple)
-                                                    .foregroundColor(.white)
-                                                    .cornerRadius(12)
-                                            }
-                                            .padding(.horizontal)
-                                            .padding(.bottom, 8)
-                                        }
-                                        .frame(maxWidth: geometry.size.width / 2, maxHeight: .infinity)
-                                        .padding()
-                                        .background(
-                                            RoundedRectangle(cornerRadius: 30)
-                                                .fill(Color.white.opacity(0.95))
-                                                .shadow(color: Color.black.opacity(0.1), radius: 20, x: 0, y: 10)
-                                        )
-                                        .padding(.bottom)
-                                    }
-                                    #endif
-                                    
-                                } else {
-                                    
-                                    ForEach(Array(store.products.enumerated()), id: \.offset) { index, plan in
-                                        VStack(spacing: 16) {
-                                            
-                                            if index == 1 {
-                                                RoundedRectangle(cornerRadius: 12)
-                                                    .fill(Color.green)
-                                                    .frame(width: 80, height: 28)
-                                                    .overlay(
-                                                        Text("-36%")
-                                                            .font(.headline)
-                                                            .foregroundStyle(.white)
-                                                        ,alignment: .center
-                                                    )
-                                            }
-                                            
-                                            Text(plan.displayName)
-                                                .font(.title2)
-                                                .fontWeight(.semibold)
-                                                .foregroundColor(.black)
-                                            
-                                            
-                                            Rectangle()
-                                                .fill(Color.yellow)
-                                                .frame(width: 60, height: 2)
-                                            
-                                            Text(plan.displayPrice)
-                                                .font(.system(size: 40, weight: .bold))
-                                                .foregroundColor(.black)
-                                            
-                                            Text(plan.description)
-                                                .foregroundColor(.gray)
-                                                .multilineTextAlignment(.center)
-                                                .lineLimit(nil)
-                                            
-                                            Button(action: {
-                                                
-                                                Task {
-                                                    do {
-                                                        let transaction = try await store.purchase(plan)
-                                                        self.onTap(.success(transaction))
-                                                    } catch {
-                                                        self.onTap(.failure(error))
-                                                    }
-                                                }
-                                            }) {
-                                                Text("Assinar")
-                                                    .fontWeight(.semibold)
-                                                    .padding()
-                                                    .frame(maxWidth: .infinity)
-                                                    .background(Color.purple)
-                                                    .foregroundColor(.white)
-                                                    .cornerRadius(12)
-                                            }
-                                            .padding(.horizontal)
-                                            .padding(.bottom, 8)
-                                        }
-                                        .frame(maxWidth: geometry.size.width / 2, maxHeight: .infinity)
-                                        .padding()
-                                        .background(
-                                            RoundedRectangle(cornerRadius: 30)
-                                                .fill(Color.white.opacity(0.95))
-                                                .shadow(color: Color.black.opacity(0.1), radius: 20, x: 0, y: 10)
-                                        )
-                                        .padding(.bottom)
-                                    }
-                                }
-                            }
-                            
-                            .padding(.horizontal)
-                        }
-                        .onAppear {
-                            // centraliza o segundo item (índice 1)
-                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                                withAnimation {
-                                    proxy.scrollTo(1, anchor: .center)
-                                }
-                            }
-                        }
-                    }
-                    
-                    Spacer(minLength: 8)
-                    
-                    Label("Cancele a qualquer momento", systemImage: "")
-                        .foregroundStyle(.white)
-                        .fontWeight(.medium)
-                }
-                
-            }
-            
-            
-        }
-    }
-    
-    private func advantages() -> some View {
-        VStack(alignment: .leading, spacing: 16) {
-            Text("Vantagens")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
-                .foregroundStyle(.white)
-            
-            HStack {
-                VStack(alignment: .leading, spacing: 16) {
-                    HStack {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(.green)
-                        Text("Cronograma personalizado")
-                            .foregroundStyle(.white)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(.green)
-                        Text("Até 10 simulados por mês")
-                            .foregroundStyle(.white)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(.green)
-                        Text("Sem anúncios")
-                            .foregroundStyle(.white)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(.green)
-                        Text("Questões por matéria ilimitado")
-                            .foregroundStyle(.white)
-                    }
-                    
-                    HStack {
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundStyle(.green)
-                        Text("Acesso a e-books, mais de 10.000 páginas")
-                            .foregroundStyle(.white)
-                    }
-                    
-                }
-                Spacer()
-            }
-            .fontWeight(.light)
-            .font(.system(size: 20))
-        }
-        .padding()
-        .background(.ultraThinMaterial.opacity(0.5))
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-    }
-}
-
-#Preview {
-#if DEBUG
-    PMKPremiumPageView(imageName: "presentation_bg", previewType: .a) {_ in
-    }
-#endif
-}
-
 fileprivate let PREVIEWS = ProcessInfo.processInfo.isSwiftUIPreview
 
 fileprivate extension ProcessInfo {
@@ -314,3 +16,136 @@ fileprivate extension ProcessInfo {
         return environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
     }
 }
+
+public struct PMKPremiumPageView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @StateObject private var store = PMKStoreFacade.shared.subscriptionService
+    
+    private let imageName: String
+    private let isIpad: Bool
+    private var onTap: (Result<StoreKit.Transaction, Error>) -> Void
+    
+#if DEBUG
+    public init(imageName: String, previewType: PreviewsMoetizationType, isIpad: Bool, onTap: @escaping (Result<StoreKit.Transaction, Error>) -> Void) {
+        self.imageName = imageName
+        self.isIpad = isIpad
+        self.onTap = onTap
+        store.previewProducts = previewType.makeProducts()
+    }
+#else
+    public init(imageName: String, isIpad: Bool, onTap: @escaping (Result<StoreKit.Transaction, Error>) -> Void) {
+        self.imageName = imageName
+        self.onTap = onTap
+        self.isIpad = isIpad
+    }
+#endif
+    
+    public var body: some View {
+        VStack(spacing: 20) {
+            
+            Text("Assinatura PasseiOAB")
+                .font(isIpad ? .largeTitle : .title3)
+                .bold()
+                .padding(.top, 8)
+            
+            Text("Escolha o plano ideal para você")
+                .font(isIpad ? .title2 : .subheadline)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            
+            VStack(spacing: 12) {
+                
+                if PREVIEWS {
+#if DEBUG
+                    ForEach(Array(store.previewProducts.enumerated()), id: \.offset) { index, product in
+                        subscriptionOption(
+                            title: product.displayName,
+                            price: product.displayPrice,
+                            description: product.description
+                        )
+                    }
+#endif
+                } else {
+                    ForEach(Array(store.products.enumerated()), id: \.offset) { index, plan in
+                        subscriptionOption(
+                            title: plan.displayName,
+                            price: plan.displayPrice,
+                            description: plan.description
+                        ).onTapGesture {
+                            Task {
+                                do {
+                                    let transaction = try await store.purchase(plan)
+                                    self.onTap(.success(transaction))
+                                } catch {
+                                    self.onTap(.failure(error))
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.top, 8)
+            
+            Button(action: {
+                
+                if !PREVIEWS {
+                    Task {
+                        await store.refreshStatus()
+                    }
+                }
+            }) {
+                Text("Restaurar")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .font(.headline)
+                    .cornerRadius(12)
+            }
+            .padding(.top, isIpad ? 20 : 12)
+            
+            Button("Cancelar") {
+                dismiss()
+            }
+            .foregroundStyle(.secondary)
+            .padding(.bottom, 16)
+            
+        }
+        .padding(.horizontal, isIpad ? 32 : 16)
+        .presentationDetents( isIpad ? [.large] : [.medium, .large])
+    }
+    
+    @ViewBuilder
+    func subscriptionOption(title: String, price: String, description: String) -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            HStack {
+                Text(title)
+                    .font(isIpad ? .title : .headline)
+                    .fontWeight(.semibold)
+                Spacer()
+                Text(price)
+                    .font(isIpad ? .title3 : .headline)
+                    .foregroundStyle(.purple)
+                    .bold()
+            }
+            Text(description)
+                .font(isIpad ? .headline : .caption)
+                .fontWeight(.regular)
+                .foregroundStyle(.secondary)
+        }
+        .padding(isIpad ? 32 : 16)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(12)
+    }
+}
+
+#Preview {
+#if DEBUG
+    PMKPremiumPageView(imageName: "presentation_bg", previewType: .a, isIpad: false) {_ in
+    }
+#endif
+}
+
